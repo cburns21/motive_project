9b14b1a0663490a0657e8b1bf7c1f1aef3a471b3
caroline burns <carolineburns@carolines-MacBook.local>
2019-06-13 13:38:32 -0600

    added background image and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>
+          Caroline Burns' Motive Project
+        </h1>
+      </div>
+      <div className='button'>
+        <button  type='button' className="btn-lg" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+          Click Me!
+        </button>
+      </div>
     </div>
   );
 }
